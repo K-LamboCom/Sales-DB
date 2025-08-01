4694e6b48209c24ae5cd622a2c500f844e81981c
KhoiiDev <leminhkhoitdtu@gmail.com>
2025-08-01 16:19:52 +0700

    first commit

---
+++ src/test/java/klambo/salesdb/SalesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package klambo.salesdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
